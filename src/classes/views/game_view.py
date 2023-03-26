5d414f9e914277bbd21cfdd94b6c61cc09f8c2db
Jeb <jesperkauppinen@gmail.com>
2023-03-27 02:14:53 +0300

    load tilemap scene and initialize guard
    Took 29 seconds

--- src/classes/views/game_view.py
+++ src/classes/views/game_view.py
@@ -1,6 +1,7 @@
 import arcade
 
 from constants import CONSTANTS as C
+from ..guard import Guard
 
 
 class GameView(arcade.View):
@@ -8,10 +9,17 @@ class GameView(arcade.View):
 
     def __init__(self):
         super().__init__()
+        self.scene = None
+        self.tilemap = None
+        self.guard = None
+        self.setup()
 
     def setup(self):
         """Set up the view."""
-        pass
+        self.tilemap = arcade.load_tilemap("assets/tilemaps/example.tilemap.json", scaling=1.25)
+        self.scene = arcade.Scene.from_tilemap(self.tilemap)
+
+        self.guard = Guard()
 
     def on_show_view(self):
         """Called when switching to this view."""
@@ -21,16 +29,30 @@ class GameView(arcade.View):
         """Draw the view."""
         self.clear()
 
+        self.scene.draw()
+
+        self.guard.draw()
+
         arcade.draw_text(
             "Files retrieved: 0/0",
             10,
             C.SCREEN_HEIGHT - 30,
             arcade.color.WHITE,
             font_size=30,
+            font_name="Minecraft",
             anchor_x="left",
             anchor_y="center",
         )
 
+    def on_update(self, delta_time: float):
+        """Update the view."""
+        self.scene.update()
+        self.guard.update(delta_time)
+
     def on_mouse_press(self, _x, _y, _button, _modifiers):
         """Handle mouse press events."""
         pass
+
+    def on_key_press(self, key, modifiers):
+        """Handle key press events."""
+        self.guard.on_key_press(key, modifiers)
