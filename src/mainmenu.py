f8d8e2a179fed1fb4e982b135fcc797533679fbf
derek_lee <20820343@student.curtin.edu.au>
2023-03-27 00:23:22 +0800

    feat: Added options and credit buttons to menu

--- src/mainmenu.py
+++ src/mainmenu.py
@@ -2,19 +2,71 @@ from ursina import *
 
 
 class Menu(Entity):
+    MENU_ORIGIN = (0, 0)
+    BTN_SCALE = (3, 0.66)
+    BTN_COLOR = color.gray
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.menu = Entity(parent=self, enabled=False)
         self.menu.disable()
 
-        Text(text="Shadow Heist", parent=self.menu, origin=(0, 0), position=(0, 2), scale=20)
-        btnPlay = Button(text="Play", parent=self.menu, origin=(0, 0), position=(0, 0), scale=(3, 0.66))
-        btnPlay.on_click = lambda: self.close()
-        btnQuit = Button(text="Quit", parent=self.menu, origin=(0, 0), position=(0, -1), scale=(3, 0.66))
-        btnQuit.on_click = lambda: application.quit()
+        Text(
+            text="Shadow Heist",
+            parent=self.menu,
+            origin=Menu.MENU_ORIGIN,
+            position=(0, 2),
+            scale=20,
+        )
+
+        btn_play = Button(
+            text="Play",
+            parent=self.menu,
+            origin=Menu.MENU_ORIGIN,
+            position=(0, 1),
+            scale=Menu.BTN_SCALE,
+            color=Menu.BTN_COLOR,
+        )
+        btn_play.on_click = lambda: self.close()
+
+        btn_options = Button(
+            text="Options",
+            parent=self.menu,
+            origin=Menu.MENU_ORIGIN,
+            position=(0, 0),
+            scale=Menu.BTN_SCALE,
+            color=Menu.BTN_COLOR,
+        )
+        btn_options.on_click = lambda: self.options()
+
+        btn_credits = Button(
+            text="Credits",
+            parent=self.menu,
+            origin=Menu.MENU_ORIGIN,
+            position=(0, -1),
+            scale=Menu.BTN_SCALE,
+            color=Menu.BTN_COLOR,
+        )
+        btn_credits.on_click = lambda: self.credits()
+
+        btn_quit = Button(
+            text="Quit",
+            parent=self.menu,
+            origin=Menu.MENU_ORIGIN,
+            position=(0, -2),
+            scale=Menu.BTN_SCALE,
+            color=Menu.BTN_COLOR,
+        )
+        btn_quit.on_click = lambda: application.quit()
 
     def open(self):
         self.menu.enable()
 
     def close(self):
-        invoke(setattr, self.menu, 'enabled', False, delay=.5)
+        invoke(setattr, self.menu, "enabled", False, delay=0.5)
+
+    def options(self):
+        pass
+
+    def credits(self):
+        pass
