c4f62506e458388c2a28a1b51c5866a96415f4f6
Jeb <jesperkauppinen@gmail.com>
2023-03-27 02:14:24 +0300

    load example fonts
    Took 22 seconds

--- src/constants.py
+++ src/constants.py
@@ -6,3 +6,7 @@ class CONSTANTS:
     SCREEN_HEIGHT = 720
     SCREEN_TITLE = "Shadow Heist v0.0.1 - PyWeek 35"
     BACKGROUND_COLOR = arcade.color.BLACK
+
+
+arcade.load_font("assets/fonts/pixel.ttf")  # MODERN WARFARE
+arcade.load_font("assets/fonts/font.ttf")  # Minecraft
