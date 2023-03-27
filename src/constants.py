06fe4651b00b7a5754dc00c3afe6e4a5deed2bc3
Jeb <jesperkauppinen@gmail.com>
2023-03-28 01:53:32 +0300

    world scale
    Took 1 hour 52 minutes

--- src/constants.py
+++ src/constants.py
@@ -6,6 +6,7 @@ class CONSTANTS:
     SCREEN_HEIGHT = 720
     SCREEN_TITLE = "Shadow Heist v0.0.1 - PyWeek 35"
     BACKGROUND_COLOR = arcade.color.BLACK
+    WORLD_SCALE = 1.2
 
 
 arcade.load_font("assets/fonts/pixel.ttf")  # MODERN WARFARE
