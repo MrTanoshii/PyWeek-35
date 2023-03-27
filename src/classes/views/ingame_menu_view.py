b64fa84c66d4298c1c2a513c7fcc2bd41fd70cc7
Derek Lee Nen That <80673848+Dereklee0312@users.noreply.github.com>
2023-03-28 00:36:18 +0800

    In Game Menu (#9)
    * feat: Added buttons to ingame_menu; Button click not implemented yet
    
    * feat: Implemented temporary on_click functions
    
    * feat: Refactored code

--- src/classes/views/ingame_menu_view.py
+++ src/classes/views/ingame_menu_view.py
@@ -1,10 +1,61 @@
 import arcade
+import arcade.gui as GUI
 
 from constants import CONSTANTS as C
 
 
 class IngameMenuView(arcade.View):
     """Base class for the 'ingame menu' view."""
+
+    def __init__(self):
+        super().__init__()
+
+        # Measurement variables
+        button_width = 300
+        button_bottom = 20
+
+        # --- Required for all code that uses UI element,
+        # a UIManager to handle the UI.
+        self.manager = GUI.UIManager()
+        self.manager.enable()
+
+        # Create veritcal BoxGroup to align buttons
+        self.v_box = GUI.UIBoxLayout()
+
+        # Create buttons
+        resume_button = GUI.UIFlatButton(text="Resume", width=button_width)
+        options_button = GUI.UIFlatButton(text="Options", width=button_width)
+        credits_button = GUI.UIFlatButton(text="Credits", width=button_width)
+        quit_button = GUI.UIFlatButton(text="Quit", width=button_width)
+
+        button_lst = [resume_button, options_button, credits_button, quit_button]
+        for button in button_lst:
+            self.v_box.add(button.with_space_around(bottom=button_bottom))
+
+        # User decorators to handle on_click events
+        @resume_button.event("on_click")
+        def on_click_resume(event):
+            print("Resume button clicked: ", event)
+
+        @options_button.event("on_click")
+        def on_click_options(event):
+            print("Options button clicked: ", event)
+
+        @credits_button.event("on_click")
+        def on_click_credits(event):
+            print("Credits button clicked: ", event)
+
+        @quit_button.event("on_click")
+        def on_click_quit(event):
+            print("Quit button clicked: ", event)
+            arcade.exit()
+
+        # Create a widget to hold the v_box widget, that will center the buttons
+        self.manager.add(
+            GUI.UIAnchorWidget(
+                anchor_x="center_x", anchor_y="center_y", child=self.v_box
+            )
+        )
 
     def on_show_view(self):
         """Called when switching to this view"""
@@ -13,15 +64,7 @@ class IngameMenuView(arcade.View):
     def on_draw(self):
         """Draw the menu"""
         self.clear()
-
-        arcade.draw_text(
-            "Menu Screen - click to advance",
-            C.SCREEN_WIDTH / 2,
-            C.SCREEN_HEIGHT / 2,
-            arcade.color.WHITE,
-            font_size=30,
-            anchor_x="center",
-        )
+        self.manager.draw()
 
     def on_mouse_press(self, _x, _y, _button, _modifiers):
         """Handle mouse press events."""
