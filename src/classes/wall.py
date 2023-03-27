cb16964370a06718e45144cb0f3bdc3fcbf831a3
Jeb <jesperkauppinen@gmail.com>
2023-03-28 01:56:39 +0300

    no need to scale walls
    Took 17 seconds

--- src/classes/wall.py
+++ src/classes/wall.py
@@ -3,7 +3,7 @@ import arcade
 
 class Wall(arcade.Sprite):
     def __init__(self, x, y, w, h):
-        super().__init__(":resources:images/tiles/boxCrate_double.png", scale=1.0)
+        super().__init__(":resources:images/tiles/boxCrate_double.png")
         self.center_x = x
         self.center_y = y
         self.width = w
