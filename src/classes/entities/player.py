f37b9ba270d8726bad6b7f154bc1f881fa0297f0
Krzysztof <irateboolean@gmx.com>
2023-03-27 22:57:16 -0400

    animated player

--- src/classes/entities/player.py
+++ src/classes/entities/player.py
@@ -1,4 +1,5 @@
 import math
+import os
 from typing import Optional
 import arcade
 from src.constants import CONSTANTS as C
@@ -48,13 +49,28 @@ class Player(arcade.Sprite):
         self.keyboard = keyboard
         self.facing_left = True
         self.last_facing = True
-        self.textures = []
         texture = arcade.load_texture(filename, flipped_horizontally=True)
-        self.textures.append(texture)
-        texture = arcade.load_texture(filename)
-        self.textures.append(texture)
+      
 
-    def update(self):
+        base_path = f"src/assets/animations/panda/"
+
+        self.texture_list_left = [
+            arcade.load_texture(f"{base_path}/{texture}", hit_box_algorithm="Simple")
+            for texture in os.listdir(base_path)
+        ]
+        
+        self.texture_list_right = [
+            arcade.load_texture(f"{base_path}/{texture}", hit_box_algorithm="Simple", flipped_horizontally=True)
+            for texture in os.listdir(base_path)
+        ]
+        self.texture_options = [self.texture_list_left, self.texture_list_right]
+
+        self.current_texture = self.texture_list_left
+        self.animation_speed = 48/60
+        self.animation_counter = 0
+        self.current_texture_index = 0
+  
+    def on_update(self, delta_time):
         move_x = ((self.keyboard["D"] | self.keyboard["RIGHT"]) * C.MOVEMENT_SPEED) - (
             (self.keyboard["A"] | self.keyboard["LEFT"]) * C.MOVEMENT_SPEED
         )
@@ -68,17 +84,26 @@ class Player(arcade.Sprite):
 
         if move_x < 0:
             self.facing_left = True
+            self.current_texture = self.texture_list_left
         elif move_x > 0:
             self.facing_left = False
+            self.current_texture = self.texture_list_right
 
         # This is to prevent the player from flipping back and forth
         if self.last_facing == self.facing_left:
             pass
         else:
-            # This is to flip the playerd
-            self.texture = self.textures[self.facing_left]
+            # This is to flip the player
+            # self.texture = self.textures[self.facing_left]
+            self.current_texture = self.texture_options[self.facing_left]
             self.last_facing = self.facing_left
 
+
+        self.animation_counter += self.animation_speed
+        if self.animation_counter > 1:
+            self.update_animation()
+            self.animation_counter = 0
+    
         ### This binds the player to the screen
         # if self.left < 0:
         #     self.left = 0
@@ -88,3 +113,12 @@ class Player(arcade.Sprite):
         #     self.bottom = 0
         # elif self.top > C.SCREEN_HEIGHT - 1:
         #     self.top = C.SCREEN_HEIGHT - 1
+
+
+
+    def update_animation(self):
+        """Update the animated texture"""
+        self.texture = self.next_item(self.current_texture, self.current_texture_index)
+    def next_item(self, lst, idx):
+        self.current_texture_index = (idx + 1) % len(lst)
+        return lst[self.current_texture_index]
