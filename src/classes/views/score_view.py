e659b0e4e7283c0aded7d7f2abd6be92b154390a
derek_lee <20820343@student.curtin.edu.au>
2023-03-28 19:07:51 +0800

    feat: Adjusted texts and positioning

--- src/classes/views/score_view.py
+++ src/classes/views/score_view.py
@@ -3,6 +3,7 @@ import arcade
 from src.classes.managers.game_manager import GameManager
 from src.constants import CONSTANTS as C
 
+
 class ScoreView(arcade.View):
     def __init__(self, game_view):
         """Base class for the 'score' view."""
@@ -17,17 +18,22 @@ class ScoreView(arcade.View):
         """Draw the game over screen"""
         self.clear()
         self._draw_centered_text("Game Over", 54, 200)
-        self._draw_centered_text(f"Score: {GameManager.instance.score}", 24, 75)
-        self._draw_centered_text(f"Completion time:", 24, 0)
-        self._draw_centered_text(f"Rating:", 24, -75)
+        self._draw_centered_text(f"Rating", 38, 100)
+        self._draw_centered_text(f"Score:", 22, -40, "right")
+        self._draw_centered_text(f" {GameManager.instance.score}", 22, -40, "left")
+        self._draw_centered_text(f"Time:", 22, -100, "right")
+        # Placeholder for Time
+        self._draw_centered_text(f" 10s", 22, -100, "left")
         self._draw_centered_text("Press any key to restart", 12, -250)
         self._draw_centered_text("Press ESC to exit", 12, -300)
 
-    def _draw_centered_text(self, text: str, font_size: int, y_offset: int):
+    def _draw_centered_text(
+        self, text: str, font_size: int, y_offset: int, anchor: str = "center"
+    ):
         """Helper function to draw text centered on the screen"""
         x = C.SCREEN_WIDTH / 2
         y = C.SCREEN_HEIGHT / 2 + y_offset
-        arcade.draw_text(text, x, y, arcade.color.WHITE, font_size, anchor_x="center")
+        arcade.draw_text(text, x, y, arcade.color.WHITE, font_size, anchor_x=anchor)
 
     def on_key_press(self, key, _modifiers):
         if key == arcade.key.ESCAPE:
