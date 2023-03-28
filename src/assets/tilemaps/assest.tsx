52c9cd7629d6203bb0e43cd5c04a4004e90e5128
mikuzi1337 <miko.markus.kauppinen@gmail.com>
2023-03-29 01:20:56 +0300

    updated map with new assets

---
+++ src/assets/tilemaps/assest.tsx
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="assest" tilewidth="144" tileheight="80" tilecount="13" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="70" height="80" source="../../../../../Documents/sereri3.png"/>
+ </tile>
+ <tile id="1">
+  <image width="144" height="80" source="../../../../../Documents/s4.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="../../../../../Documents/g4.png"/>
+ </tile>
+ <tile id="4">
+  <image width="32" height="32" source="../../../../../Documents/a1.png"/>
+ </tile>
+ <tile id="7">
+  <image width="32" height="32" source="../../../../../Documents/3.png"/>
+ </tile>
+ <tile id="9">
+  <image width="32" height="32" source="../../../../../Documents/g3ggs3.png"/>
+ </tile>
+ <tile id="14">
+  <image width="32" height="32" source="../../../../../Documents/g33.png"/>
+ </tile>
+ <tile id="15">
+  <image width="32" height="32" source="../../../../../Documents/g34.png"/>
+ </tile>
+ <tile id="16">
+  <image width="32" height="32" source="../../../../../Documents/g31.png"/>
+ </tile>
+ <tile id="17">
+  <image width="32" height="32" source="../../../../../Documents/g32.png"/>
+ </tile>
+ <tile id="18">
+  <image width="32" height="32" source="../../../../../Documents/g3ggs4.png"/>
+ </tile>
+ <tile id="19">
+  <image width="32" height="32" source="../../../../../Documents/g3ggs1.png"/>
+ </tile>
+ <tile id="20">
+  <image width="32" height="32" source="../../../../../Documents/g3ggs2.png"/>
+ </tile>
+</tileset>
