61e81f4ebecb4a5f828ea848db27ff2eea1b8c12
mikuzi1337 <miko.markus.kauppinen@gmail.com>
2023-03-29 12:55:40 +0300

    Added tile textures

--- src/assets/tilemaps/assest.tsx
+++ src/assets/tilemaps/assest.tsx
@@ -1,43 +1,46 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.0" name="assest" tilewidth="144" tileheight="80" tilecount="13" columns="0">
+<tileset version="1.10" tiledversion="1.10.0" name="assest" tilewidth="144" tileheight="80" tilecount="14" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="0">
-  <image width="70" height="80" source="../../../../../Documents/sereri3.png"/>
+ <tile id="21">
+  <image width="32" height="32" source="../tiles/g32.png"/>
  </tile>
- <tile id="1">
-  <image width="144" height="80" source="../../../../../Documents/s4.png"/>
+ <tile id="22">
+  <image width="32" height="32" source="../tiles/g33.png"/>
  </tile>
- <tile id="2">
-  <image width="32" height="32" source="../../../../../Documents/g4.png"/>
+ <tile id="23">
+  <image width="32" height="32" source="../tiles/g34.png"/>
  </tile>
- <tile id="4">
-  <image width="32" height="32" source="../../../../../Documents/a1.png"/>
+ <tile id="24">
+  <image width="32" height="32" source="../tiles/g31.png"/>
  </tile>
- <tile id="7">
-  <image width="32" height="32" source="../../../../../Documents/3.png"/>
+ <tile id="25">
+  <image width="70" height="80" source="../tiles/sereri3.png"/>
  </tile>
- <tile id="9">
-  <image width="32" height="32" source="../../../../../Documents/g3ggs3.png"/>
+ <tile id="26">
+  <image width="144" height="80" source="../tiles/s4.png"/>
  </tile>
- <tile id="14">
-  <image width="32" height="32" source="../../../../../Documents/g33.png"/>
+ <tile id="27">
+  <image width="32" height="32" source="../tiles/g4.png"/>
  </tile>
- <tile id="15">
-  <image width="32" height="32" source="../../../../../Documents/g34.png"/>
+ <tile id="28">
+  <image width="32" height="32" source="../tiles/g3ggs1.png"/>
  </tile>
- <tile id="16">
-  <image width="32" height="32" source="../../../../../Documents/g31.png"/>
+ <tile id="29">
+  <image width="32" height="32" source="../tiles/g3ggs2.png"/>
  </tile>
- <tile id="17">
-  <image width="32" height="32" source="../../../../../Documents/g32.png"/>
+ <tile id="30">
+  <image width="32" height="32" source="../tiles/g3ggs3.png"/>
  </tile>
- <tile id="18">
-  <image width="32" height="32" source="../../../../../Documents/g3ggs4.png"/>
+ <tile id="31">
+  <image width="32" height="32" source="../tiles/g3ggs4.png"/>
  </tile>
- <tile id="19">
-  <image width="32" height="32" source="../../../../../Documents/g3ggs1.png"/>
+ <tile id="32">
+  <image width="32" height="32" source="../tiles/3.png"/>
  </tile>
- <tile id="20">
-  <image width="32" height="32" source="../../../../../Documents/g3ggs2.png"/>
+ <tile id="33">
+  <image width="32" height="32" source="../tiles/a1.png"/>
+ </tile>
+ <tile id="34">
+  <image width="32" height="32" source="../tiles/g35.png"/>
  </tile>
 </tileset>
