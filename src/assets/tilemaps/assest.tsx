1abeb5e2895c85b18339860c10df3f1f124ec6f0
mikuzi1337 <miko.markus.kauppinen@gmail.com>
2023-03-29 17:14:21 +0300

    Added more tile textures

--- src/assets/tilemaps/assest.tsx
+++ src/assets/tilemaps/assest.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.0" name="assest" tilewidth="144" tileheight="80" tilecount="14" columns="0">
+<tileset version="1.10" tiledversion="1.10.0" name="assest" tilewidth="144" tileheight="96" tilecount="32" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="21">
   <image width="32" height="32" source="../tiles/g32.png"/>
@@ -42,5 +42,59 @@
  </tile>
  <tile id="34">
   <image width="32" height="32" source="../tiles/g35.png"/>
+ </tile>
+ <tile id="37">
+  <image width="32" height="32" source="../../../../../Documents/mud1.png"/>
+ </tile>
+ <tile id="38">
+  <image width="32" height="32" source="../../../../../Documents/mud2.png"/>
+ </tile>
+ <tile id="39">
+  <image width="32" height="32" source="../../../../../Documents/mud3.png"/>
+ </tile>
+ <tile id="40">
+  <image width="32" height="32" source="../../../../../Documents/mud4.png"/>
+ </tile>
+ <tile id="41">
+  <image width="32" height="32" source="../../../../../Documents/mud5.png"/>
+ </tile>
+ <tile id="42">
+  <image width="32" height="32" source="../../../../../Documents/mud6.png"/>
+ </tile>
+ <tile id="43">
+  <image width="32" height="32" source="../../../../../Documents/mud7.png"/>
+ </tile>
+ <tile id="44">
+  <image width="32" height="32" source="../../../../../Documents/mud8.png"/>
+ </tile>
+ <tile id="45">
+  <image width="32" height="32" source="../../../../../Documents/mud9.png"/>
+ </tile>
+ <tile id="46">
+  <image width="32" height="32" source="../../../../../Documents/mud10.png"/>
+ </tile>
+ <tile id="47">
+  <image width="32" height="32" source="../../../../../Documents/mud12.png"/>
+ </tile>
+ <tile id="48">
+  <image width="32" height="32" source="../../../../../Documents/mud11.png"/>
+ </tile>
+ <tile id="49">
+  <image width="32" height="32" source="../../../../../Documents/mud15.png"/>
+ </tile>
+ <tile id="50">
+  <image width="32" height="32" source="../../../../../Documents/mud14.png"/>
+ </tile>
+ <tile id="51">
+  <image width="32" height="32" source="../../../../../Documents/mud13.png"/>
+ </tile>
+ <tile id="52">
+  <image width="32" height="32" source="../../../../../Documents/wall.png"/>
+ </tile>
+ <tile id="53">
+  <image width="32" height="32" source="../../../../../Documents/wall2.png"/>
+ </tile>
+ <tile id="54">
+  <image width="64" height="96" source="../../../../../Documents/compuuter.png"/>
  </tile>
 </tileset>
