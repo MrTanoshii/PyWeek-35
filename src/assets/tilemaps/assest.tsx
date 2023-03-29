8d8639b5665d0b94154a524bfbb75ca4081d0f5e
mikuzi1337 <miko.markus.kauppinen@gmail.com>
2023-03-29 19:48:23 +0300

    Fixed map

--- src/assets/tilemaps/assest.tsx
+++ src/assets/tilemaps/assest.tsx
@@ -1,106 +1,82 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.0" name="assest" tilewidth="144" tileheight="96" tilecount="34" columns="0">
+<tileset version="1.10" tiledversion="1.10.0" name="assest" tilewidth="144" tileheight="96" tilecount="26" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="21">
-  <image width="32" height="32" source="../tiles/g32.png"/>
- </tile>
- <tile id="22">
-  <image width="32" height="32" source="../tiles/g33.png"/>
- </tile>
- <tile id="23">
-  <image width="32" height="32" source="../tiles/g34.png"/>
- </tile>
- <tile id="24">
-  <image width="32" height="32" source="../tiles/g31.png"/>
- </tile>
- <tile id="25">
-  <image width="70" height="80" source="../tiles/sereri3.png"/>
- </tile>
- <tile id="26">
-  <image width="144" height="80" source="../tiles/s4.png"/>
- </tile>
- <tile id="27">
-  <image width="32" height="32" source="../tiles/g4.png"/>
- </tile>
- <tile id="28">
-  <image width="32" height="32" source="../tiles/g3ggs1.png"/>
- </tile>
- <tile id="29">
-  <image width="32" height="32" source="../tiles/g3ggs2.png"/>
- </tile>
- <tile id="30">
-  <image width="32" height="32" source="../tiles/g3ggs3.png"/>
- </tile>
- <tile id="31">
-  <image width="32" height="32" source="../tiles/g3ggs4.png"/>
- </tile>
- <tile id="32">
+ <tile id="57">
   <image width="32" height="32" source="../tiles/3.png"/>
  </tile>
- <tile id="33">
+ <tile id="58">
   <image width="32" height="32" source="../tiles/a1.png"/>
  </tile>
- <tile id="34">
+ <tile id="59">
+  <image width="64" height="96" source="../tiles/compuuter.png"/>
+ </tile>
+ <tile id="60">
+  <image width="32" height="32" source="../tiles/fgf.png"/>
+ </tile>
+ <tile id="61">
+  <image width="32" height="32" source="../tiles/fgf1.png"/>
+ </tile>
+ <tile id="62">
+  <image width="32" height="32" source="../tiles/g3ggs1.png"/>
+ </tile>
+ <tile id="63">
+  <image width="32" height="32" source="../tiles/g3ggs2.png"/>
+ </tile>
+ <tile id="64">
+  <image width="32" height="32" source="../tiles/g3ggs3.png"/>
+ </tile>
+ <tile id="65">
+  <image width="32" height="32" source="../tiles/g3ggs4.png"/>
+ </tile>
+ <tile id="66">
+  <image width="32" height="32" source="../tiles/g4.png"/>
+ </tile>
+ <tile id="67">
+  <image width="32" height="32" source="../tiles/g31.png"/>
+ </tile>
+ <tile id="68">
+  <image width="32" height="32" source="../tiles/g32.png"/>
+ </tile>
+ <tile id="69">
+  <image width="32" height="32" source="../tiles/g33.png"/>
+ </tile>
+ <tile id="70">
+  <image width="32" height="32" source="../tiles/g34.png"/>
+ </tile>
+ <tile id="71">
   <image width="32" height="32" source="../tiles/g35.png"/>
  </tile>
- <tile id="37">
-  <image width="32" height="32" source="../../../../../Documents/mud1.png"/>
+ <tile id="72">
+  <image width="32" height="32" source="../tiles/mud1.png"/>
  </tile>
- <tile id="38">
-  <image width="32" height="32" source="../../../../../Documents/mud2.png"/>
+ <tile id="73">
+  <image width="32" height="32" source="../tiles/mud10.png"/>
  </tile>
- <tile id="39">
-  <image width="32" height="32" source="../../../../../Documents/mud3.png"/>
+ <tile id="74">
+  <image width="32" height="32" source="../tiles/mud11.png"/>
  </tile>
- <tile id="40">
-  <image width="32" height="32" source="../../../../../Documents/mud4.png"/>
+ <tile id="75">
+  <image width="32" height="32" source="../tiles/mud12.png"/>
  </tile>
- <tile id="41">
-  <image width="32" height="32" source="../../../../../Documents/mud5.png"/>
+ <tile id="76">
+  <image width="32" height="32" source="../tiles/mud13.png"/>
  </tile>
- <tile id="42">
-  <image width="32" height="32" source="../../../../../Documents/mud6.png"/>
+ <tile id="77">
+  <image width="32" height="32" source="../tiles/mud14.png"/>
  </tile>
- <tile id="43">
-  <image width="32" height="32" source="../../../../../Documents/mud7.png"/>
+ <tile id="78">
+  <image width="32" height="32" source="../tiles/mud15.png"/>
  </tile>
- <tile id="44">
-  <image width="32" height="32" source="../../../../../Documents/mud8.png"/>
+ <tile id="79">
+  <image width="144" height="80" source="../tiles/s4.png"/>
  </tile>
- <tile id="45">
-  <image width="32" height="32" source="../../../../../Documents/mud9.png"/>
+ <tile id="80">
+  <image width="70" height="80" source="../tiles/sereri3.png"/>
  </tile>
- <tile id="46">
-  <image width="32" height="32" source="../../../../../Documents/mud10.png"/>
+ <tile id="81">
+  <image width="32" height="32" source="../tiles/wall.png"/>
  </tile>
- <tile id="47">
-  <image width="32" height="32" source="../../../../../Documents/mud12.png"/>
- </tile>
- <tile id="48">
-  <image width="32" height="32" source="../../../../../Documents/mud11.png"/>
- </tile>
- <tile id="49">
-  <image width="32" height="32" source="../../../../../Documents/mud15.png"/>
- </tile>
- <tile id="50">
-  <image width="32" height="32" source="../../../../../Documents/mud14.png"/>
- </tile>
- <tile id="51">
-  <image width="32" height="32" source="../../../../../Documents/mud13.png"/>
- </tile>
- <tile id="52">
-  <image width="32" height="32" source="../../../../../Documents/wall.png"/>
- </tile>
- <tile id="53">
-  <image width="32" height="32" source="../../../../../Documents/wall2.png"/>
- </tile>
- <tile id="54">
-  <image width="64" height="96" source="../../../../../Documents/compuuter.png"/>
- </tile>
- <tile id="55">
-  <image width="32" height="32" source="../../../../../Documents/fgf.png"/>
- </tile>
- <tile id="56">
-  <image width="32" height="32" source="../../../../../Documents/fgf1.png"/>
+ <tile id="82">
+  <image width="32" height="32" source="../tiles/wall2.png"/>
  </tile>
 </tileset>
