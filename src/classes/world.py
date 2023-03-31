03ad7def23aea1a4d6986dd34b6cc845b7a97621
Jeb <76889226+Jeb4dev@users.noreply.github.com>
2023-03-31 12:38:42 -0700

    removed unused function

--- src/classes/world.py
+++ src/classes/world.py
@@ -48,6 +48,3 @@ class World:
 
     def tiled_to_screen(self, x: int, y: int) -> tuple[int, int]:
         return x * C.WORLD_SCALE, (self.height * self.tile_size - y) * C.WORLD_SCALE
-
-    def tiled_to_opengl(self, x: int, y: int) -> tuple[int, int]:
-        return x * C.WORLD_SCALE, y * C.WORLD_SCALE
