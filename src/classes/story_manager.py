e3ee65d69f1bb83fd065a952eb4e971df76f0944
Jeb <jesperkauppinen@gmail.com>
2023-04-02 16:29:31 +0300

    play story every time open safe
    Took 11 minutes

--- src/classes/story_manager.py
+++ src/classes/story_manager.py
@@ -10,16 +10,7 @@ class StoryManager:
 
     def __init__(self):
         self.story = [
-            "Oh, Hello Player, Welcome to the game. Press SPACE to continue.",
-            "You have to steal the missing data from the datacenter of our rival company.",
-            "The datacenter is located in the basement of the building.",
-            "You have to get there undetected.",
-            "Good luck, Agent Whiskers.",
-            "You need to shut down the lights, so the guards won't see you.",
-            "You can do that by pressing the E.",
-            "You can also use the E to open the safes.",
-            "You can use the WASD keys to move.",
-            "Okay then, good luck, Agent Whiskers.",
+            "Oh, Hello Player, Welcome to the Secret Mission. Press SPACE to Continue.",
         ]
         self.current_story_index = -1
         self.special_story = {
@@ -84,4 +75,9 @@ class StoryManager:
             if not self.special_story[story]["played"]:
                 self.special_story[story]["played"] = True
                 return self.special_story[story]["story"]
+        return None
+
+    def play_story(self, story):
+        if self.special_story.get(story):
+            return self.special_story[story]["story"]
         return None
